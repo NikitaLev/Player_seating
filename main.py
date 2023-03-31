9d753e32aae2b88f26bb728850f097f650953ac2
NikitaLev <nikita.leventev97@gmail.com>
2023-03-31 13:28:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+
+test_players = ['Fenix',
+           'Sol',
+           'BOSS',
+           'Пончик',
+           'Гаечка',
+           'Miks',
+           'Trauma',
+           'Школьник',
+           'Кринж',
+           'Рокфеллер']
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(test_players)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
